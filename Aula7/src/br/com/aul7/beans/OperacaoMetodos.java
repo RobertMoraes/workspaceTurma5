b9e50c6e7f14b7b00b8f5b74e6492c504ef90808
RobertMoraes <robert.moraes@outlook.com>
2021-11-10 19:42:36 -0300

    Atualizacao do projeto

---
+++ Aula7/src/br/com/aul7/beans/OperacaoMetodos.java
@@ -0,0 +1,8 @@
+package br.com.aul7.beans;
+
+public interface OperacaoMetodos {
+	
+	void pesquisar(); 
+	void salvar(); 
+	void excluir();
+}
---
+++ Aula7/src/br/com/aul7/beans/Paciente.java
@@ -0,0 +1,30 @@
+package br.com.aul7.beans;
+
+public class Paciente extends Pessoa {
+
+	//Construtor
+	public Paciente(String nome, String cpf, String fone, String endereco) {
+		super(nome, cpf, fone, endereco);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void pesquisar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void salvar() {
+		System.out.println("SAlvar Paciente");
+		
+	}
+
+	@Override
+	public void excluir() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
